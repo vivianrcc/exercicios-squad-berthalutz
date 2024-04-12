013f773eba55bd268fbad8e8711f98876a5e3040
marianachoratto <marianachoratto@gmail.com>
2024-04-11 23:11:51 -0300

    fix vercel 3

--- Projeto Final/base/settings.py
+++ Projeto Final/base/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-o&v056tb&5v8p&g9k2_ph-htr4m9(1%8bo_eqal76o5!n!ec#8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
