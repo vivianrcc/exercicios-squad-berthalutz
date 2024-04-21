b9cf0373ca32f81f8e18fc2db7c5fda3dd8f9f96
vivianrcc <vivianrosanacc@gmail.com>
2024-04-21 17:33:05 -0300

    criei o botão Ainda esta com problemas ?......

--- Projeto Final/base/settings.py
+++ Projeto Final/base/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, "blog/static")]
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "blog/static")]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 STATIC_URL = "static/"
 
